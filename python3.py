27c46fec7ba6af9b5a8d0bd92d8de0f679c094f6
LeishaChoudhary <leishachoudhary121@gmail.com>
2025-03-21 23:26:04 +0530

    {hi i am leisha }

--- python3.py
+++ python3.py
@@ -19,20 +19,54 @@
 #string12="this is leisha choudhary .\ti am 20 yrs old"
 #print(string12)
 
-str1="apna"
-str2="college"
-print(str1+str2)
-len1=len(str1)
-print(len1)
+# tr1="apna"
+# sstr2="college"
+# print(str1+str2)
+# len1=len(str1)
+# print(len1)
 
-str= "apnacollege"
-print(str[1:4])
+# str= "apnacollege"
+# print(str[1:4])
 
-print(str[ :4])
-print(str[1: ])
+# print(str[ :4])
+# print(str[1: ])
 
-print(str[-3:-1])
-print(str.endswith("ege"))
-print(str.replace("college","ghar"))
+# print(str[-3:-1])
+# print(str.endswith("ege"))
+# print(str.replace("college","ghar"))
+
+# print(str.find("apna"))
+# print(str.count("co"))
+# print(str.count("co"))
+# print(str.capitalize())
+# print(str.capitalize())
+# fname=input("enter your first name")
+# print(("hello:",fname))
+# print(len(str(fname)))
+
+# print(fname.count("$"))
+
+
+age=17
+if ( age>=18 ):
+    print("can vote")
+elif(age<=15):
+    print("can go to school")
+elif(age==16):
+    print("can dance")
+else:
+    (print("He is 17"))
+
+    X=int(input(("enter a  value")))
+    
+    print(X)
+
+
+    if((X%2==0)):
+        print("even")
+    else:
+        print("odd")
+        
+
 
 
--- readme.md
+++ readme.md
@@ -1,6 +1,7 @@
-git init
+git init {one time only}
 <!-- git add README.md -->
-git commit -m "first commit"
+git add .
+git commit -m "{this is a comment}"
 git branch -M main
-git remote add origin https://github.com/L-eisha/PythonIn30days.git
-git push -u origin main
+git remote add origin https://github.com/L-eisha/PythonIn30days.git " {one time only}
+git push -u origin main/master
