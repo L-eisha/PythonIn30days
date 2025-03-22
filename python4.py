b8aa7eef8428b257bf44ea8b9a0427dbf9e521ac
LeishaChoudhary <leishachoudhary121@gmail.com>
2025-03-22 23:25:57 +0530

    {this is a comment}

---
+++ python4.py
@@ -0,0 +1,141 @@
+#questions
+#1
+# num=int(input("enter a number"))
+# print(num)
+# if(num%2==0):
+#     print("even")
+
+# else:
+#     print("odd")
+#     #2
+
+#     Q=123
+#     print(Q)
+#     if(Q%2==0):
+#         print("even")
+#     else:
+#         print("odd")
+
+#3
+# Q=1
+# W=3
+# E=5
+
+# if(W>Q and W>E):
+#      print(W)
+# elif(Q>W and Q> E):
+#     print(Q)
+# elif(E>W and E>Q):
+#     print(E)
+
+#     #4
+# a=34
+# print(a)
+# if(a%7==0):
+#         print("multiple of 7")
+# else:
+#         print("not divisible by 7")
+
+      #  Day2 question practice
+
+Q=4
+# print(type(Q))
+
+# a="leisha"
+# print(type("leisha"))
+# print(len(a))
+# b="choudhary "
+# print(len(b))
+
+# n1=4
+# n2=5
+# print(n1+n2)
+# diff=n1-n2
+# print(diff)
+# pro=(n1*n2)
+# print(pro)
+# div=(n1/n2)
+# print(div)
+# print(n2%n2)
+# print(n1**n2)
+# print(n1//n2)
+
+# r=int(input(("enter radius")))
+# pi=3.14
+# area=((pi* r**2))
+# print(area)
+
+# print('Floating Point Number, PI', 3.14)
+# print('Floating Point Number, gravity', 9.81)
+
+# age=int(20)
+# print(age)
+
+# height=float(165.5)
+# print(height)
+
+# a=4+2j
+# print(a)
+
+# base=10
+# height=10
+# area=(0.5*base*height)
+# print(area)
+
+#input
+# base1=int(input("enterbase"))
+# print(base1)
+
+# height1=int(input("enter height"))
+# print(height1)
+# area=(0.5*height1* base1)
+# print(area)
+
+# a1=3
+# a2=6
+# perimeter=(2*a1+2*a2)
+# print(perimeter)
+
+# #8 (later)
+# x1=2
+# y1=2
+# x2=6
+# y2=10
+
+# slope=((y2-y1)/(x2-x1))
+# print(slope)
+
+# x=4
+# y=(x**2+6*x+9)
+# print(y)
+# x=-3
+# y=(x**2+6*x+9)
+# print(y)
+
+# p=("python")
+# print(len(p))#falsy statement
+
+#13
+
+str="I hope this course is not full of jargon"
+print(str.find("jargon"))
+
+f=7//3
+print(float(f))
+
+in1=9.8
+print(int(in1))
+
+h= int(40)
+rate=int(28)
+salary=int( h*rate)
+print(salary)
+
+years=1000
+secound=365*24*60*60
+yearlived=(1000*365*24*60*60)
+print(yearlived)
+
+
+
+
