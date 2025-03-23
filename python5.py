74093b79626349e009ec25fe0d33d0c191a7b3bb
LeishaChoudhary <leishachoudhary121@gmail.com>
2025-03-23 23:25:07 +0530

    list in python

---
+++ python5.py
@@ -0,0 +1,25 @@
+#list
+# marks=[20,30,40,54,60]
+# print(marks)
+# print(len(marks))
+# print(marks[3])
+
+# student=[20,'karan', 45 ,24]
+# print(student[1])
+# student[1]="dhruv"
+# print(student)
+
+no=[1,2,3,4,5,6]
+# print(no[1:4])
+# print(len(no))
+
+print(no.append(68))
+print(no)
+print(no.sort())
+print(no)
+print(no.sort(reverse=True))
+print(no)
+print(no.reverse())
+print(no)
+print(no.insert(3,9))
+print(no)
