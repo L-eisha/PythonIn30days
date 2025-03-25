e81330562b6430b720d9cbb1f6a90627879a7d89
LeishaChoudhary <leishachoudhary121@gmail.com>
2025-03-25 19:25:43 +0530

    revision

---
+++ python6.py
@@ -0,0 +1,71 @@
+#revision
+print('hello world')
+name='leisha'
+print(name)
+a=2
+b=3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a%b)
+print(a//b)
+print(a/b)
+print(a!=b)
+print(a==b)
+print(a**b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+print(type(a))
+print(type(name))
+
+#this is acomment
+#ctrl+/=comment out
+
+v1=5
+v2=8
+print("and operator:" ,v1 and v2)
+print("or operator:" , v1 or v2)
+print(len(name))
+print(name.index)
+
+c=float(v2)
+
+# batch=input("enter your batch year;")
+# print(batch)
+# branch=input('in what branch u are:')
+# print(branch)
+
+space=" "
+last="choudhary"
+print(name + space + last)
+print(name[1])
+
+print(name[1:])
+print(last[4:6])
+print(last[-1:-5])
+
+print(last.endswith("er"))
+print(name.replace("sha","shi"))
+print(name.capitalize())
+print(last.find("dh"))
+print(last.count("a"))
+
+#conditional
+a=5
+if( a==7):
+    print("yes")
+else:
+    print("no")
+
+w=4
+e=5
+u=5
+if(w==4):
+    
+ if(e==u):
+    print("done")
+
+    #area
