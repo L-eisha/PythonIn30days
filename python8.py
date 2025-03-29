6e26eccc637f9aea09b50f0dda0a8c5de83d6b22
LeishaChoudhary <leishachoudhary121@gmail.com>
2025-03-29 21:55:51 +0530

    list questions

---
+++ python8.py
@@ -0,0 +1,30 @@
+#question
+l1=[]
+print(l1)
+print(type(l1))
+
+l2=[1,2,3,4,5]
+print(l2)
+
+print(len(l2))
+print(l2[0])
+print(l2[2])
+print(l2[4])
+
+l3=["age" , 4 , "delhi", 4.44]
+print(l3)
+
+list=["google","microsoft", "netflix" ,"meta"]
+print(len(list))
+print(type(list))
+list[1]="adobe" 
+print(list)
+
+present="google" in list # for checking existence
+print(present)
+
+print(list.append("nykaa"))
+print(list)
+
+list.insert(2,"make my trip")
+print(list)
