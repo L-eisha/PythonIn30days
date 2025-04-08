cbc8d191b446f45ccbc3deeb7e65d6f31de6c8bc
LeishaChoudhary <leishachoudhary121@gmail.com>
2025-04-08 21:34:56 +0530

    loops

--- python14.py
+++ python14.py
@@ -41,4 +41,33 @@ numbers = [0, 1, 2, 3, 4, 5]
 for number in numbers: # number is temporary name to refer to the list's items, valid only inside this loop
     print(number)       # the numbers will be printed line by line, from 0 to 5
 
-    
+language = 'Python'
+for letter in language:
+    print(letter)
+
+
+for i in range(len(language)):
+    print(language[i])
+
+numbers = (0, 1, 2, 3, 4, 5)
+for number in numbers:
+    print(number)
+
+    person = {
+    'first_name':'himalya',
+    'last_name':'hill'
+    '',
+    'age':2500,
+    'country':'india',
+    'is_marred':False,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+    }
+}
+for key in person:
+    print(key)
+
+for key, value in person.items():
+    print(key, value) # this way we get both keys and values printed out
