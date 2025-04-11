de6d5ab5086515854e2938aeceaa3790f1100690
LeishaChoudhary <leishachoudhary121@gmail.com>
2025-04-11 23:44:26 +0530

    functions

--- python15.py
+++ python15.py
@@ -39,3 +39,33 @@ def add_two_numbers ():
     total = num_one + num_two
     return total
 print(add_two_numbers())
+
+
+def greetings (name):
+    message = name + ', welcome to Python for Everyone!'
+    return message
+
+print(greetings('Asabeneh'))
+
+def add_ten(num):
+    ten = 10
+    return num + ten
+print(add_ten(90))
+
+def square_number(x):
+    return x * x
+print(square_number(2))
+
+def area_of_circle (r):
+    PI = 3.14
+    area = PI * r ** 2
+    return area
+print(area_of_circle(10))
+
+def sum_of_numbers(n):
+    total = 0
+    for i in range(n+1):
+        total+=i
+    print(total)
+print(sum_of_numbers(10)) # 55
+print(sum_of_numbers(100)) # 5050
