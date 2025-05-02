f2723a83e34c0937bb61de694f26f47c3fc66013
LeishaChoudhary <leishachoudhary121@gmail.com>
2025-05-02 21:20:57 +0530

    revision

---
+++ python17.py
@@ -0,0 +1,40 @@
+# FULL REVISION TILL NOW
+print("HELLO WORLD" ,"LEISHA HERE!")
+# operations and type
+a1=24
+a2=89
+print(a1+a2)
+print(a1-a2)
+print(a1*a2)
+print(a1**a2)
+print(a1!=a2)
+print(a1==a2)
+print(a1>=a2)
+print(a1<a2)
+print(type(a2))
+a1=a1+1
+print(a1)
+a2=str(a2)
+print(type(a2))
+# HELLO WORLD LEISHA HERE!
+# 113
+# -65
+# 2136
+# 689922823738748289569678722457303368025854318182334628509535763762267553432378023689439264783448453642383249654228109492224
+# True
+# False
+# False
+# True
+# <class 'int'>
+# 25
+# <class 'str'>
+
+
+# input
+# 6
+# 36
+r=int(input("enter radius"))
+pi=3.14
+area=int(pi*(r*r ))
+print(area)
+
