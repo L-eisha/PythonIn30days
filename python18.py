83192f5d740cd639e0da6c4eb9a44eabd5fae1a1
LeishaChoudhary <leishachoudhary121@gmail.com>
2025-05-06 23:18:52 +0530

    function

--- python18.py
+++ python18.py
@@ -59,3 +59,35 @@ def add_two_numbers ():
 add_two_numbers()
 # Leisha Choudhary
 # 58
+
+def greetings (name = 'Leisha'):
+    message = name + ', welcome to Python for Everyone!'
+    return message
+print(greetings())
+print(greetings('Choudhary'))
+
+def generate_full_name (first_name = 'Leisha', last_name = 'Choudhary'):
+    space = ' '
+    full_name = first_name + space + last_name
+    return full_name
+
+print(generate_full_name())
+print(generate_full_name('muskan','riya'))
+
+def calculate_age (birth_year,current_year = 2021):
+    age = current_year - birth_year
+    return age;
+print('Age: ', calculate_age(1821))
+
+def weight_of_object (mass, gravity = 9.81):
+    weight = str(mass * gravity)+ ' N' # the value has to be changed to string first
+    return weight
+print('Weight of an object in Newtons: ', weight_of_object(100)) # 9.81 - average gravity on Earth's surface
+print('Weight of an object in Newtons: ', weight_of_object(100, 1.62)) # gravity on the surface of the Moon
+# Leisha, welcome to Python for Everyone!
+# Choudhary, welcome to Python for Everyone!
+# Leisha Choudhary
+# muskan riya
+# Age:  200
+# Weight of an object in Newtons:  981.0 N
+# Weight of an object in Newtons:  162.0 N
